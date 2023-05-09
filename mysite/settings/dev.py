39685d4ecf39bb6daac2e9c9fdbb0734b254e8b6
ammar29135 <ayeshashabbir91@gmail.com>
2023-05-09 21:08:00 +0500

    conent scheduling

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-s3a2hp&50a0r2b0iv5fp)pvlv4+23igh&rdqm=l99f8^@b3#fy'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
